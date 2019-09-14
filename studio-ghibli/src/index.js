a744012b948c81fdaac64a53467a37a700d83783
Diana Grams <deegrams221@gmail.com>
2019-09-14 15:16:47 -0600

    setup src/index.js with Provider, createStore, applyMiddleware, compose, thunk, and imported Reducer

--- studio-ghibli/src/index.js
+++ studio-ghibli/src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {reducer} from './Store/Reducers';
 
+// for redux dev tools chrome extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// createStore pass in reducer, composeEnhancers for middleware(thunk)
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  rootElement
+);
