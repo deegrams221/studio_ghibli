0c7c8e9a688324fd74b8a5be4f1806cc6a4a56c7
Diana Grams <deegrams221@gmail.com>
2019-09-14 16:50:27 -0600

    updated actions

--- studio-ghibli/src/Store/Actions/index.js
+++ studio-ghibli/src/Store/Actions/index.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 export const FETCH_FILM_START = 'FETCH_FILM_START';
 export const FETCH_FILM_SUCCESS = 'FETCH_FILM_SUCCESS';
 export const FETCH_FILM_FAIL = 'FETCH_FILM_FAIL';
+export const FETCH_SEARCH_START = 'FETCH_SEARCH_START';
+export const FETCH_SEARCH_SUCCESS = 'FETCH_SEARCH_SUCCESS';
+export const FETCH_SEARCH_FAIL = 'FETCH_SEARCH_FAIL';
 
 // action creators
 export const getFilm = () => dispatch => {
@@ -20,4 +23,14 @@ export const getFilm = () => dispatch => {
       console.log('catch error:', error.res.message);
       dispatch({type: FETCH_FILM_FAIL, payload: error.res.message})
     })
+};
+
+export const getSearch = () => dispatch => {
+  dispatch({type: FETCH_SEARCH_START});
+  axios
+    .get(``)
+    .then(res => {
+      dispatch({type: FETCH_SEARCH_SUCCESS, payload: res.data})
+    })
+    .catch(error => ({type: FETCH_SEARCH_FAIL, payload: error.data.message}))
 }
